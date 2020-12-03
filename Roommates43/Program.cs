0cde905174b4d45fcf5acbf1355dc1e7ce13bbb7
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2020-12-03 15:53:58 -0600

    get all rooms and get room by id

---
+++ Roommates43/Program.cs
@@ -0,0 +1,102 @@
+﻿using Roommates43.Models;
+using Roommates43.Repositories;
+using System;
+using System.Collections.Generic;
+
+namespace Roommates43
+{
+    class Program
+    {
+        //  This is the address of the database.
+        //  We define it here as a constant since it will never change.
+        private const string CONNECTION_STRING = @"server=localhost\SQLExpress;database=Roommates;integrated security=true";
+
+        static void Main(string[] args)
+        {
+            RoomRepository roomRepo = new RoomRepository(CONNECTION_STRING);
+
+            bool runProgram = true;
+            while (runProgram)
+            {
+                string selection = GetMenuSelection();
+
+                switch (selection)
+                {
+                    case ("Show all rooms"):
+                        ShowAllRooms(roomRepo);
+                        break;
+                    case ("Search for room"):
+                        ShowOneRoom(roomRepo);
+                        break;
+                    case ("Add a room"):
+                        // Do stuff
+                        break;
+                    case ("Exit"):
+                        runProgram = false;
+                        break;
+                }
+            }
+
+        }
+
+        static void ShowAllRooms(RoomRepository roomRepo)
+        {
+            List<Room> rooms = roomRepo.GetAll();
+            foreach (Room r in rooms)
+            {
+                Console.WriteLine($"[{r.Id}] {r.Name} Max Occ({r.MaxOccupancy})");
+            }
+            Console.Write("Press any key to continue");
+            Console.ReadKey();
+        }
+
+        static void ShowOneRoom(RoomRepository roomRepo)
+        {
+            Console.Write("Room Id: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Room room = roomRepo.GetById(id);
+
+            Console.WriteLine($"{room.Id} - {room.Name} Max Occupancy({room.MaxOccupancy})");
+            Console.Write("Press any key to continue");
+            Console.ReadKey();
+        }
+
+        static string GetMenuSelection()
+        {
+            Console.Clear();
+
+            List<string> options = new List<string>()
+        {
+            "Show all rooms",
+            "Search for room",
+            "Add a room",
+            "Exit"
+        };
+
+            for (int i = 0; i < options.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {options[i]}");
+            }
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine();
+                    Console.Write("Select an option > ");
+
+                    string input = Console.ReadLine();
+                    int index = int.Parse(input) - 1;
+                    return options[index];
+                }
+                catch (Exception)
+                {
+
+                    continue;
+                }
+            }
+
+        }
+    }
+}
---
+++ Roommates43/Repositories/RoomRepository.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates43.Models;
+using System.Collections.Generic;
+
+namespace Roommates43.Repositories
+{
+    /// <summary>
+    ///  This class is responsible for interacting with Room data.
+    ///  It inherits from the BaseRepository class so that it can use the BaseRepository's Connection property
+    /// </summary>
+    public class RoomRepository : BaseRepository
+    {
+        /// <summary>
+        ///  When new RoomRespository is instantiated, pass the connection string along to the BaseRepository
+        /// </summary>
+        public RoomRepository(string connectionString) : base(connectionString) { }
+
+        // ...We'll add some methods shortly...
+        public List<Room> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Name, MaxOccupancy FROM Room";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Room> rooms = new List<Room>();
+
+                    while (reader.Read())
+                    {
+                        int idColumnPosition = reader.GetOrdinal("Id");
+
+                        int idValue = reader.GetInt32(idColumnPosition);
+
+                        int nameColumnPosition = reader.GetOrdinal("Name");
+                        string nameValue = reader.GetString(nameColumnPosition);
+
+                        int maxOccupancyColumnPosition = reader.GetOrdinal("MaxOccupancy");
+                        int maxOccupancy = reader.GetInt32(maxOccupancyColumnPosition);
+
+                        Room room = new Room
+                        {
+                            Id = idValue,
+                            Name = nameValue,
+                            MaxOccupancy = maxOccupancy,
+                        };
+
+                        rooms.Add(room);
+                    }
+
+                    reader.Close();
+
+                    return rooms;
+                }
+            }
+        }
+
+        public Room GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Name, MaxOccupancy FROM Room WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Room room = null;
+
+                    // If we only expect a single row back from the database, we don't need a while loop.
+                    if (reader.Read())
+                    {
+                        room = new Room
+                        {
+                            Id = id,
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            MaxOccupancy = reader.GetInt32(reader.GetOrdinal("MaxOccupancy")),
+                        };
+                    }
+
+                    reader.Close();
+
+                    return room;
+                }
+            }
+        }
+
+        public void Insert(Room room)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Room (Name, MaxOccupancy) 
+                                         OUTPUT INSERTED.Id 
+                                         VALUES (@name, @maxOccupancy)";
+                    cmd.Parameters.AddWithValue("@name", room.Name);
+                    cmd.Parameters.AddWithValue("@maxOccupancy", room.MaxOccupancy);
+                    int id = (int)cmd.ExecuteScalar();
+
+                    room.Id = id;
+                }
+            }
+
+            // when this method is finished we can look in the database and see the new room.
+        }
+    }
+}
